8972161188f152d3061b4a24daf7c7022d21b6d4
ghali <ghali.merzoug@gmail.com>
2018-10-08 21:12:49 +0200

    update stack

--- scripts/delete-vpn-conf.py
+++ scripts/delete-vpn-conf.py
@@ -28,7 +28,7 @@ def create_paloalto_config(peer_group,vpn_connection_id):
 
     config_text.append('configure \n')
     config_text.append('edit network virtual-router default protocol bgp')
-    config_text.append('edit peer-group {}'.format(peer_group_name))
+    config_text.append('edit peer-group {}'.format(peer_group))
     config_text.append('delete peer {}'.format(fdata[0]))
     config_text.append('delete network tunnel ipsec {}'.format(fdata[1]))  
     config_text.append('delete network interface tunnel units {}'.format(fdata[2]))
